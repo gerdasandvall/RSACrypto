0fa0ee39a4074fbcdfbcb79756ce36a8261125f2
gerdasandvall <122353900+gerdasandvall@users.noreply.github.com>
2023-11-16 12:16:24 +0100

    Initial commit

---
+++ RSACrypto/UserControl1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace RSACrypto
+{
+
+
+
+
+    public partial class Backdrop: UserControl
+    {
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        byte[] plaintext;
+        byte[] encryptedtext;
+
+        public Backdrop()
+        {
+            InitializeComponent();
+        }
+
+        //encryption function
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+
+
+        //decryption function
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            plaintext = ByteConverter.GetBytes(txtplain.Text);
+            encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
+            txtencrypt.Text = ByteConverter.GetString(encryptedtext);
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] decryptedtex = Decryption(encryptedtext,
+            RSA.ExportParameters(true), false);
+            txtdecrypt.Text = ByteConverter.GetString(decryptedtex);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            txtplain.Clear();
+           txtencrypt.Clear();
+            txtdecrypt.Clear();
+
+        }
+
+
+      
+
+        private void Backdrop_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        }
+
+
+     
+        }
+
+     
+   
+
